d93f973975233ce5212fbd2e5c95bd54e1da24d0
Subhajit Bag <u2subhajit@gmail.com>
2026-04-12 02:55:14 +0530

    feat: configure axios base URL and credentials in main entry point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+import axios from "axios";
+
+axios.defaults.baseURL = "http://13.126.242.51:5000";
+axios.defaults.withCredentials = true;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
